ca488afc875a23bef24a4ba184f36a997657a294
dengtao37 <dengtao37@bgyfw.com>
2024-12-25 20:11:56 +0800

    导入插件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,5 +5,10 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
+    <bep-button type="primary">底色按钮</bep-button>
+    <bep-button type="secondary">边框按钮</bep-button>
+    <bep-button type="dashed">虚线按钮</bep-button>
+    <bep-button type="danger">警示按钮</bep-button>
+    <bep-button type="text">文字按钮</bep-button>
   </main>
 </template>
